791981b7c02c6885286fd6d85e9044df6a539352
afontainec <afontaine@uc.cl>
2021-08-02 18:27:55 -0400

    feat(deploy): add cname

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-# echo 'thewall.accionet.net' > CNAME
+echo 'thewall.accionet.net' > CNAME
 
 git init
 git add -A
